e11aac67c2f927ab67c50d3b72ca82ebefc81600
JERLOORXHCK <jerloorking@gmail.com>
2026-08-29 23:53:24 +0100

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+// JERLOORXHCK Supabase configuration
+const SUPABASE_URL = "https://pkjzasopatdfkdewgncn.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_pAgEsXYsZzr7inhRwgjd4w_5kVSkn1A";
